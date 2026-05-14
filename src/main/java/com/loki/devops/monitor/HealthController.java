6a3bcc506f4b8e37c4ac3c70451fbc7b2a1dd90e
Lokesh <bomminenilokesh01@gmail.com>
2026-05-14 15:47:53 +0530

    added-changes

--- src/main/java/com/loki/devops/monitor/HealthController.java
+++ src/main/java/com/loki/devops/monitor/HealthController.java
@@ -2,26 +2,40 @@ package com.loki.devops.monitor;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import oshi.SystemInfo;
 import oshi.hardware.CentralProcessor;
+
 import java.util.Map;
 
 @RestController
 public class HealthController {
+
     private final SystemInfo si = new SystemInfo();
 
+    // Home Page API
+    @GetMapping("/")
+    public String home() {
+        return "Realtime Health Monitor Running Successfully";
+    }
+
+    // Realtime System Stats API
     @GetMapping("/api/realtime-stats")
     public Map<String, Object> getStats() {
+
         CentralProcessor processor = si.getHardware().getProcessor();
-        // Get CPU load over 1 second interval
+
+        // CPU usage percentage
         double load = processor.getSystemCpuLoad(1000) * 100;
+
+        // Available memory
         long memory = si.getHardware().getMemory().getAvailable();
 
         return Map.of(
-            "cpu_usage_percent", String.format("%.2f", load),
-            "available_memory_bytes", memory,
-            "status", "Healthy",
-            "timestamp", System.currentTimeMillis()
+                "cpu_usage_percent", String.format("%.2f", load),
+                "available_memory_bytes", memory,
+                "status", "Healthy",
+                "timestamp", System.currentTimeMillis()
         );
     }
 }
